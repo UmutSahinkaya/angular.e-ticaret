2eb0d7f41a7fde93813d3511e12e4ce2667ecbce
Umut <umut.dev@example.com>
2026-02-23 15:41:14 +0300

    Content kısmını ayarlıyalım

--- e-ticaret/apps/admin/src/pages/layouts/layouts.ts
+++ e-ticaret/apps/admin/src/pages/layouts/layouts.ts
@@ -6,7 +6,7 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import Breadcrumb from './breadcrumb';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { navigations } from '../../navigation';
 import { NavPipe } from '../../pipes/nav-pipe';
 import { FormsModule } from '@angular/forms';
@@ -19,7 +19,8 @@ import { DatePipe } from '@angular/common';
     RouterLinkActive,
     NavPipe,
     FormsModule,
-    DatePipe
+    DatePipe,
+    RouterOutlet
   ],
   templateUrl: './layouts.html',
   encapsulation: ViewEncapsulation.None,

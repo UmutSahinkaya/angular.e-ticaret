5ccd4c08a7f5898c4757e60cb3223a57e9cc8750
Umut <umut.dev@example.com>
2026-02-25 09:44:49 +0300

    (Refactoring) Route yapısını iyileştirelim

--- e-ticaret/apps/admin/src/app.routes.ts
+++ e-ticaret/apps/admin/src/app.routes.ts
@@ -1,25 +1,22 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-    {
-        path: 'login',
-        loadComponent: () => import('./pages/login/login')
-    },
-    {
-        path:"",
-        loadComponent:()=>import("./pages/layouts/layouts"),
-        children:[{
-            path:"",
-            loadComponent:()=>import("./pages/home/home")
-        },
-        {
-            path: "products",
-            loadComponent:()=>import("./pages/products/products")   
-        },
-        {
-            path:"products/create",
-            loadComponent:()=>import("./pages/products/create/create")
-        }
-    ]
-    }
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login'),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./pages/layouts/layouts'),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home'),
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('./pages/products/routes'),
+      }
+    ],
+  },
 ];

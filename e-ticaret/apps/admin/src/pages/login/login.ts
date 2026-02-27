dc9dd6568cf4530323d7efa441b75310c354a1c6
Umut <umut.dev@example.com>
2026-02-27 11:09:04 +0300

    Login sayfasını oluşturalım

--- e-ticaret/apps/admin/src/pages/login/login.ts
+++ e-ticaret/apps/admin/src/pages/login/login.ts
@@ -1,11 +1,33 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FlexiToastService } from 'flexi-toast';
+import { UserModel } from '../users/users';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Login {
+  readonly #http = inject(HttpClient);
+  readonly #toast = inject(FlexiToastService);
+  readonly #router = inject(Router);
 
+  signIn(form:NgForm){
+    if(!form.valid) return;
+     const endpoint = `api/users?userName=${form.value['userName']}&password=${form.value['password']}`;
+     this.#http.get<UserModel[]>(endpoint).subscribe({
+      next: (users) => {
+        if(users.length > 0){
+          this.#toast.showToast('Giriş Başarılı', 'success');
+          localStorage.setItem('user', JSON.stringify(users[0]));
+          this.#router.navigate(['/']);
+        } else {
+          this.#toast.showToast('Geçersiz kullanıcı adı veya şifre', 'error');
+        }
+     }});
+  }
 }

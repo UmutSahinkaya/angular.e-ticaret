785bac09696102f5e611eb62db34652bb258e043
Umut <umut.dev@example.com>
2026-02-27 11:32:35 +0300

    json-server beta sürümüyle alakalı olan bir sorundan dolayı password filtering yapıyordu versiyon değiştirildi şuan login de problem yok

--- e-ticaret/apps/admin/src/pages/login/login.ts
+++ e-ticaret/apps/admin/src/pages/login/login.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { FlexiToastService } from 'flexi-toast';
 import { UserModel } from '../users/users';
+import { FlexiToastService } from 'flexi-toast';
+import { Router } from '@angular/router';
 
 @Component({
   imports: [FormsModule],
@@ -16,18 +21,22 @@ export default class Login {
   readonly #toast = inject(FlexiToastService);
   readonly #router = inject(Router);
 
-  signIn(form:NgForm){
-    if(!form.valid) return;
-     const endpoint = `api/users?userName=${form.value['userName']}&password=${form.value['password']}`;
-     this.#http.get<UserModel[]>(endpoint).subscribe({
-      next: (users) => {
-        if(users.length > 0){
-          this.#toast.showToast('Giriş Başarılı', 'success');
-          localStorage.setItem('user', JSON.stringify(users[0]));
-          this.#router.navigate(['/']);
-        } else {
-          this.#toast.showToast('Geçersiz kullanıcı adı veya şifre', 'error');
-        }
-     }});
+  signIn(form: NgForm) {
+    if (!form.valid) return;
+
+    const endpoint = `api/users?userName=${form.value['userName']}&password=${form.value['password']}`;
+    this.#http.get<UserModel[]>(endpoint).subscribe((res) => {
+      if (res.length === 0) {
+        this.#toast.showToast(
+          'Hata',
+          'Kullanıcı adı ya da şifre yanlış',
+          'error',
+        );
+        return;
+      }
+
+      localStorage.setItem('response', JSON.stringify(res[0]));
+      this.#router.navigateByUrl('/');
+    });
   }
 }

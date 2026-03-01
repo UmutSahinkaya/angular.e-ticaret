bbc1fe608207ce407c9d6a338fe54b0ff003a5ba
Umut <umut.dev@example.com>
2026-03-01 11:49:20 +0300

    Ödeme yapalım ve sipariş oluşturalım

--- e-ticaret/apps/ui/src/pages/payments/payments.ts
+++ e-ticaret/apps/ui/src/pages/payments/payments.ts
@@ -1,27 +1,47 @@
+import { initialOrder, OrderModel } from '@shared/models/order.model';
 import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BasketModel } from '@shared/models/basket.model';
 import { TrCurrencyPipe } from 'tr-currency';
 import { Common } from '../../services/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-  imports: [RouterLink,TrCurrencyPipe],
+  imports: [RouterLink, TrCurrencyPipe, FormsModule],
   templateUrl: './payments.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Payments {
-  readonly result=httpResource<BasketModel[]>(()=>`api/baskets?userId=${this.#common.user()!.id}`);
-  readonly data=computed(()=>this.result.value() ?? []);
-  readonly total=computed(()=>{
-    let val=0;
-    this.data().forEach(res=>{
-      val+=res.productPrice * res.quantity;
-    })
+  readonly result = httpResource<BasketModel[]>(
+    () => `api/baskets?userId=${this.#common.user()!.id}`,
+  );
+  readonly baskets = computed(() => this.result.value() ?? []);
+  readonly total = computed(() => {
+    let val = 0;
+    this.baskets().forEach((res) => {
+      val += res.productPrice * res.quantity;
+    });
     return val;
-  })
-  readonly kdv=computed(()=>this.total() * 0.18);
-  
+  });
+  readonly kdv = computed(() => this.total() * 0.18);
+  readonly data = signal<OrderModel>(initialOrder);
+  readonly showSuccessPart = signal<boolean>(false);
+
   readonly #common = inject(Common);
+  readonly #http = inject(HttpClient);
+
+  pay(form: NgForm) {
+    if (!form.valid) return;
+
+    this.data.update((prev) => ({
+      ...prev,
+      userId: this.#common.user()!.id!,
+      baskets: [...this.baskets()],
+    }));
+    this.#http.post('api/orders', this.data()).subscribe((res) => {
+      this.showSuccessPart.set(true);
+    });
+  }
 }

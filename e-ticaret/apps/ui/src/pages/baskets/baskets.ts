ced03c1ddec20f4d2d58f63c22f181065ffd24b3
Umut <umut.dev@example.com>
2026-03-01 10:43:58 +0300

    Sepetteki ürün adedini artırıp azaltalım

--- e-ticaret/apps/ui/src/pages/baskets/baskets.ts
+++ e-ticaret/apps/ui/src/pages/baskets/baskets.ts
@@ -1,8 +1,9 @@
-import { httpResource } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
 import { BasketModel } from '@shared/models/basket.model';
 import { TrCurrencyPipe } from 'tr-currency';
 import { Common } from '../../services/common';
+import { FlexiToastService } from 'flexi-toast';
 
 @Component({
   imports: [TrCurrencyPipe],
@@ -26,4 +27,29 @@ export default class Baskets {
   })
   readonly kdv=computed(()=>this.totalPrice() * 0.18);
   readonly #common = inject(Common);
+  readonly #http=inject(HttpClient);
+  readonly #toast=inject(FlexiToastService);
+
+  increament(val :BasketModel){
+    val.quantity++;
+    this.#http.put(`api/baskets/${val.id}`,val).subscribe(()=>{
+      this.result.reload();
+    });
+  }
+  decrement(val: BasketModel){
+    const count = val.quantity - 1;
+
+    if(count <= 0){
+      this.#toast.showSwal("Sil?","Ürünü sepetten silmek istiyor musunuz?","Sil",() => {
+        this.#http.delete(`api/baskets/${val.id}`).subscribe(() => {
+          this.result.reload();
+        });
+      })
+    }else{
+      val.quantity--;
+      this.#http.put(`api/baskets/${val.id}`,val).subscribe(() => {
+        this.result.reload();
+      });
+    }
+  }
 }
